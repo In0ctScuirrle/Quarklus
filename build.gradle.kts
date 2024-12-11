04871c01a1eca2105941b8d71c95455acd660e6e
Will Blanchard <Blanchardpwilliam3@gmail.com>
2024-12-11 23:05:49 +0100

    First Commit: Added OpenGL via LWJGL, yeah this not going to lie I could do it in swing, but swing has some uhh... Limitations.
    Additions |
    
    - Graphics and Audio Libraries
    
    Major Updates |
    
    - Just in build.gradle.kts
    
    Menial Updates |
    
    NONE
    
    Removals |
    
    NONE
    
    Patches |
    
    NONE

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,42 @@ plugins {
 group = "lu.embellishedduck"
 version = "PRE-ALPHA"
 
+//=======================
+// INSTANTIATE VARIABLES
+//=======================
+val lwjglVersion = "3.3.4"
+val jomlVersion = "1.10.7"
+val `joml-primitivesVersion` = "1.10.0"
+val `lwjgl3-awtVersion` = "0.1.8"
+val steamworks4jVersion = "1.9.0"
+val `steamworks4j-serverVersion` = "1.9.0"
+
+val lwjglNatives = Pair(
+    System.getProperty("os.name")!!,
+    System.getProperty("os.arch")!!
+).let { (name, arch) ->
+    when {
+        arrayOf("Linux", "SunOS", "Unit").any { name.startsWith(it) } ->
+            if (arrayOf("arm", "aarch64").any { arch.startsWith(it) })
+                "natives-linux${if (arch.contains("64") || arch.startsWith("armv8")) "-arm64" else "-arm32"}"
+            else if (arch.startsWith("ppc"))
+                "natives-linux-ppc64le"
+            else if (arch.startsWith("riscv"))
+                "natives-linux-riscv64"
+            else
+                "natives-linux"
+        arrayOf("Mac OS X", "Darwin").any { name.startsWith(it) }     ->
+            "natives-macos${if (arch.startsWith("aarch64")) "-arm64" else ""}"
+        arrayOf("Windows").any { name.startsWith(it) }                ->
+            if (arch.contains("64"))
+                "natives-windows${if (arch.startsWith("aarch64")) "-arm64" else ""}"
+            else
+                "natives-windows-x86"
+        else                                                                            ->
+            throw Error("Unrecognized or unsupported platform. Please set \"lwjglNatives\" manually")
+    }
+}
+
 
 //================================
 // REPOSITORIES TO PULL FROM HERE
@@ -24,6 +60,32 @@ repositories {
 //======================
 dependencies {
 
+    implementation(platform("org.lwjgl:lwjgl-bom:$lwjglVersion"))
 
+    implementation("org.lwjgl", "lwjgl")
+    implementation("org.lwjgl", "lwjgl-assimp")
+    implementation("org.lwjgl", "lwjgl-cuda")
+    implementation("org.lwjgl", "lwjgl-glfw")
+    implementation("org.lwjgl", "lwjgl-ktx")
+    implementation("org.lwjgl", "lwjgl-meshoptimizer")
+    implementation("org.lwjgl", "lwjgl-nuklear")
+    implementation("org.lwjgl", "lwjgl-openal")
+    implementation("org.lwjgl", "lwjgl-opencl")
+    implementation("org.lwjgl", "lwjgl-opengl")
+    implementation("org.lwjgl", "lwjgl-stb")
+    runtimeOnly("org.lwjgl", "lwjgl", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-assimp", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-glfw", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-ktx", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-meshoptimizer", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-nuklear", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-openal", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-opengl", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-stb", classifier = lwjglNatives)
+    implementation("org.joml", "joml", jomlVersion)
+    implementation("org.joml", "joml-primitives", `joml-primitivesVersion`)
+    implementation("org.lwjglx", "lwjgl3-awt", `lwjgl3-awtVersion`)
+    implementation("com.code-disaster.steamworks4j", "steamworks4j", steamworks4jVersion)
+    implementation("com.code-disaster.steamworks4j", "steamworks4j-server", `steamworks4j-serverVersion`)
 
 }
