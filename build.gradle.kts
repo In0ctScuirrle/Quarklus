df8c081e9faedaa1ebed71cfb114c980fc87ee23
Will Blanchard <Blanchardpwilliam3@gmail.com>
2025-01-22 12:29:02 +0100

    Initial Commit

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+
+    id("java")
+
+}
+
+group = "lu.embellishedduck"
+version = "PRE-ALPHA"
+
+//=====================
+// IMPORT REPOSITORIES
+//=====================
+repositories {
+
+    mavenCentral()
+
+}
+
+
+//======================
+// PROJECT DEPENDENCIES
+//======================
+dependencies {
+
+
+
+}
